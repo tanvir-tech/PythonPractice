724d5fe8c8bf17f6d0186f9c2d4df4ed0a19ee35
tanvir ahmed <tanvirah325@gmail.com>
2021-01-29 10:47:44 +0600

    python inputs

---
+++ 4_Input-Output/1_split_multiple_input.py
@@ -0,0 +1,9 @@
+a,b,c = map(int,input("1.Enter 3 Integer by space:").split())
+
+print("a+b+c =",a+b+c)
+a,b = b,a
+print("Swapping a,b => ",a,b)
+
+
+s = input("2.Enter any String :")
+print(s,type(s))
--- 4_Input-Output/take_int.py
+++
@@ -1,4 +0,0 @@
-a,b = map(int,input("Enter 2 Integer by space:").split())
-
-print("a+b =",a+b)
-
